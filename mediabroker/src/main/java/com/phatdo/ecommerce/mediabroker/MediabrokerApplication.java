e6e597663535124e5855ad5362aee0e9a707e075
tuPC <phatdtgcs220340@fpt.edu.vn>
2024-12-27 04:05:27 +0700

    [MB] - skeleton init

---
+++ mediabroker/src/main/java/com/phatdo/ecommerce/mediabroker/MediabrokerApplication.java
@@ -0,0 +1,13 @@
+package com.phatdo.ecommerce.mediabroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediabrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediabrokerApplication.class, args);
+	}
+
+}
